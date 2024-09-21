3e18295f113d1e3980f97c159d042f26bcc3f5ee
Eliecerh <51102557+Eliecerh@users.noreply.github.com>
2024-09-20 20:38:13 -0500

    Cambio en la conexion a la base de datos

--- db.php
+++ db.php
@@ -8,6 +8,6 @@ try {
     $conn = new PDO("mysql:host=$host;dbname=$db", $user, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
-    echo "Error de conexión: " . $e->getMessage();
+    echo "Connection error: " . $e->getMessage();
 }
 ?>
